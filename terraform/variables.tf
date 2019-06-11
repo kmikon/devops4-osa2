143e01b69a342252c74b3364fe01978052ff10a3
Kimmo Mikonranta <kimmo.mikonranta@edu.amiedu.fi>
2019-06-11 11:16:33 +0300

    terroform konf osa2

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {}
+variable "billing_account" {}
+variable "region" {
+  default = "europe-north1"
+}
+variable "machine_type" {
+  default = "f1-micro"
+}
+variable "network_cidr" {
+  default = "10.11.12.0/24"
+}
+
+variable "cloudflare_email" {}
+variable "cloudflare_token" {}
+variable "cloudflare_domain" {}
